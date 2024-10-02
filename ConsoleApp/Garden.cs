8db010b3c3d308d7f60055bcb4a1bd35f7c9e47f
Paweł Łęczycki <pawel@msd.info.pl>
2024-10-02 10:20:44 +0200

    Garden

---
+++ ConsoleApp/Garden.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp;
+public class Garden
+{
+    public int Size { get; }
+    private ICollection<string> Items { get; }
+
+    public Garden(int size)
+    {
+        Size = size;
+        Items = [];
+    }
+
+    public bool Plant(string name)
+    {
+        if (name == null)
+            throw new ArgumentNullException(nameof(name));
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Roślina musi posiadać nazwę", nameof(name));
+
+        if (Items.Count >= Size)
+            return false;
+
+        if (Items.Contains(name))
+        {
+            name += (Items.Count(x => x.StartsWith(name)) + 1);
+        }
+
+        Items.Add(name);
+
+        return true;
+    }
+
+    public IEnumerable<string> GetPlants()
+    {
+        return Items.ToList();
+    }
+}
