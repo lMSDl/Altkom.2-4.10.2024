6d4e8fd7227efc25b27f0eae3e45251697e10996
Paweł Łęczycki <pawel@msd.info.pl>
2024-10-03 11:25:13 +0200

    Testowanie zdarzeń

---
+++ ConsoleApp.Test.xUnit/LoggerTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture;
+
+namespace ConsoleApp.Test.xUnit
+{
+    public class LoggerTest
+    {
+        [Fact]
+        public void Log_AnyInput_EventInvokedOnce()
+        {
+            //Arrange
+            var logger = new Logger();
+            var log = new Fixture().Create<string>();
+            int result = 0;
+            int expectedResult = 1;
+
+            logger.MessageLogged += (s, e) => { result++; };
+
+            //Act
+            logger.Log(log);
+
+
+            //Assert
+            Assert.Equal(expectedResult, result); 
+        }
+
+        [Fact]
+        public void Log_AnyInput_ValidLoggerEventArgs()
+        {
+            //Arrange
+            var logger = new Logger();
+            var log = new Fixture().Create<string>();
+            object? sender = null;
+            Logger.LoggerEventArgs? loggerEventArgs = null;
+
+            logger.MessageLogged += (s, e) => { sender = s; loggerEventArgs = e as Logger.LoggerEventArgs; };
+            DateTime timeStart = DateTime.Now;
+
+            //Act
+            logger.Log(log);
+
+            //Assert
+            DateTime timeStop = DateTime.Now;
+            Assert.Equal(logger, sender);
+            Assert.NotNull(loggerEventArgs);
+            Assert.Equal(log, loggerEventArgs.Message);
+            Assert.InRange(loggerEventArgs.DateTime, timeStart, timeStop);
+        }
+
+    }
+}
---
+++ ConsoleApp/Logger.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp
+{
+    public class Logger
+    {
+        private Dictionary<DateTime, string> _logs = new Dictionary<DateTime, string>();
+
+        public event EventHandler<EventArgs>? MessageLogged;
+
+        public void Log(string message)
+        {
+            var dateTime = DateTime.Now;
+            _logs[dateTime] = message;
+
+            MessageLogged?.Invoke(this, new LoggerEventArgs(dateTime, message));
+        }
+
+        public Task<string> GetLogsAsync(DateTime from, DateTime to)
+        {
+            return Task.Run(() => string.Join("\n", _logs.Where(x => x.Key >= from).Where(x => x.Key <= to)
+            .Select(x => $"{x.Key.ToShortDateString()} {x.Key.ToShortTimeString()}: {x.Value}")));
+        }
+
+        public class LoggerEventArgs(DateTime dateTime, string message) : EventArgs
+        {
+            public DateTime DateTime => dateTime;
+            public string Message => message;
+        }
+    }
+}
